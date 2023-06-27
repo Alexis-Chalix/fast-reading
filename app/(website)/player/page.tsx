5ee8d9c2dc982a79707422a7aca8c08f574c83ec
Alexis Chalande <chalande.alexis@gmail.com>
2023-06-27 12:16:12 +0200

    New default text

--- app/(website)/player/page.tsx
+++ app/(website)/player/page.tsx
@@ -51,55 +51,75 @@ const useStyles = createStyles((theme) => ({
 
 export default function Page() {
 	const text = [
-		'+ La "Rapid Serial Visual Presentation" (RSVP), ou "Présentation ' +
-		'visuelle en série rapide" en français, est une méthode pour présenter des informations, généralement des ' +
-		'mots, très rapidement et dans un ordre séquentiel. Cette méthode est souvent utilisée dans la recherche en ' +
-		'psychologie cognitive pour étudier la perception visuelle, l\'attention et la mémoire de travail. Le ' +
-		'principe est assez simple. Imaginons que vous lisiez un texte. Au lieu d\'avoir tout le texte devant vous, ' +
-		'chaque mot (ou chaque petit groupe de mots) est présenté séparément à un endroit fixe de l\'écran, ' +
-		'généralement au centre, et ce, à une vitesse que vous pouvez souvent contrôler. Une fois qu\'un mot est ' +
-		'présenté, il est rapidement remplacé par le suivant. Ainsi, vous n\'avez pas besoin de bouger les yeux pour ' +
-		'lire, ce qui peut potentiellement augmenter votre vitesse de lecture et votre compréhension. C\'est une ' +
-		'méthode qui a été utilisée dans divers domaines, de la recherche en psychologie à l\'amélioration de la ' +
-		'vitesse de lecture. C\'est aussi la technologie derrière certaines applications de lecture rapide, comme ' +
-		'Spritz. La RSVP a ses avantages et ses inconvénients. Par exemple, elle peut aider à augmenter la vitesse ' +
-		'de lecture, mais elle peut aussi rendre difficile le retour en arrière pour relire quelque chose. De plus, ' +
-		'certaines personnes peuvent trouver la méthode fatigante ou désorientante.',
-		'+ La lecture rapide est une compétence qui peut être très utile pour absorber rapidement de grandes quantités ' +
-		'd\'informations. Voici quelques techniques populaires de lecture rapide : ' +
-		'Balayage : Cette méthode consiste à passer rapidement en revue le texte pour identifier les points clés ou ' +
-		'les informations importantes. ' +
-		'Saut : Cette technique implique de sauter consciemment certains mots ou phrases qui sont moins importantes ' +
-		'pour la compréhension globale du texte. ' +
-		'Chunking : Il s\'agit de regrouper des mots ensemble pour les lire en blocs plutôt que mot par mot. ' +
-		'Minimiser la subvocalisation : La subvocalisation est la pratique consistant à "dire" les mots dans votre ' +
-		'tête lorsque vous lisez. Cela peut ralentir considérablement votre vitesse de lecture. En apprenant à ' +
-		'minimiser la subvocalisation, vous pouvez augmenter votre vitesse de lecture. ' +
-		'Méta-guidage : Cette technique utilise un pointeur, comme votre doigt ou un stylo, pour guider vos yeux ' +
-		'pendant que vous lisez.Lecture en diagonale : Cette technique consiste à lire en diagonale, ' +
-		'en se concentrant sur les mots clés ' +
-		'et les phrases, pour obtenir une compréhension globale du texte. RSVP (Rapid Serial Visual Presentation) : ' +
-		'Cette méthode présente les mots un à un à un emplacement fixe, ' +
-		'éliminant ainsi le besoin de mouvements oculaires. Cela peut augmenter considérablement la vitesse de ' +
-		'lecture. En ce qui concerne la vitesse de lecture, elle varie énormément en fonction des personnes et de leur ' +
-		'entraînement. Une vitesse de lecture moyenne pour un adulte est d\'environ 200 à 300 mots par minute. Avec ' +
-		'la pratique et l\'utilisation de ces techniques de lecture rapide, certaines personnes peuvent atteindre ' +
-		'des vitesses de 1000 mots par minute ou plus. Cependant, il est important de noter que la compréhension ' +
-		'peut diminuer à mesure que la vitesse de lecture augmente. En ce qui concerne la méthode RSVP, il ' +
-		'n\'existe pas de données concrètes sur le record du monde pour la vitesse de lecture la plus rapide. ' +
-		'Cependant, on sait que cette méthode peut permettre d\'atteindre des ' +
-		'vitesses de lecture de plus de 1000 mots par minute. La moyenne avec la RSVP dépend beaucoup de ' +
-		'l\'entraînement individuel, mais après un entraînement conséquent, certains lecteurs peuvent atteindre une ' +
-		'vitesse de 500 à 700 mots par minute tout en maintenant une compréhension raisonnable. Il est important de ' +
-		'souligner que la lecture rapide n\'est pas seulement une question de vitesse, mais ' +
-		'aussi de compréhension. Si vous lisez à une vitesse où vous ne comprenez pas l\'information, cela ne sera ' +
-		'pas très utile. Par conséquent, la clé est de trouver un équilibre entre vitesse et compréhension.',
-		'hehe hehe hehe hehe hehe hehe hehe hehe hehe hehe hehe',
-		'wtf wtf wtf wtf wtf wtf wtf wtf wtf wtf wtf'
+		"+ La Rapid Serial Visual Presentation (RSVP) est une méthode de présentation d'information où les " +
+		"éléments sont affichés séquentiellement à un rythme rapide, généralement au même emplacement spatial. " +
+		"Cette technique est largement utilisée pour améliorer l'efficacité de la lecture rapide et pour la " +
+		"recherche en psychologie cognitive. La RSVP tire ses origines des travaux de recherche en psychologie de " +
+		"la perception des années 1960 et 1970, lorsqu'il a été démontré que les individus pouvaient identifier un " +
+		"objet parmi d'autres présentés rapidement et dans une seule position spatiale. Cette découverte a mené au " +
+		"développement de systèmes de présentation visuelle rapide utilisés dans de nombreux domaines allant de " +
+		"l'éducation à la technologie. Au cours de ce chapitre, nous explorerons le concept de RSVP, son importance " +
+		"et le contexte historique de son développement.",
+
+		"+ Le fonctionnement de la RSVP repose sur le concept de \"fenêtre d'attention\", une période pendant " +
+		"laquelle le cerveau est capable de traiter des informations visuelles sans distraction. Pendant une séance " +
+		"de RSVP, les mots ou les images sont présentés un à un à un rythme rapide, généralement au même endroit " +
+		"sur l'écran. Le temps d'exposition de chaque élément varie généralement entre 100 et 500 millisecondes. Ce " +
+		"rythme soutenu permet d'optimiser l'efficacité de la lecture ou de la perception visuelle, en réduisant " +
+		"les mouvements oculaires inutiles et en concentrant l'attention de l'observateur. La plupart des systèmes " +
+		"de RSVP permettent également à l'utilisateur de contrôler la vitesse de présentation, offrant une " +
+		"flexibilité pour s'adapter à différentes capacités de lecture et de perception.",
+
+		"+ La RSVP a trouvé des applications dans de nombreux domaines. Tout d'abord, elle est largement utilisée " +
+		"dans les techniques de lecture rapide. En affichant les mots un à un à un emplacement fixe, la RSVP réduit " +
+		"les mouvements oculaires saccadiques, augmentant ainsi la vitesse de lecture tout en conservant une bonne " +
+		"compréhension. En outre, la RSVP est un outil précieux pour la recherche en psychologie cognitive. Elle " +
+		"permet aux chercheurs d'étudier l'attention, la perception, et la mémoire visuelle en contrôlant " +
+		"précisément ce que les participants voient et quand ils le voient. Enfin, la RSVP est utilisée dans les " +
+		"interfaces homme-machine, en particulier dans les systèmes où l'espace d'affichage est limité, comme les " +
+		"lunettes de réalité augmentée ou les écrans de téléphones portables. Par exemple, une application de " +
+		"lecture rapide pour smartphone pourrait utiliser la RSVP pour afficher un livre entier sur un petit écran, " +
+		"un mot à la fois.",
+
+		"+ Au fil des ans, de nombreuses études ont été menées sur la RSVP, cherchant à comprendre et à améliorer " +
+		"cette technique. Les recherches ont démontré que, bien que la RSVP puisse augmenter considérablement la " +
+		"vitesse de lecture, elle peut également entraîner une diminution de la compréhension, en particulier à des " +
+		"vitesses très élevées. De plus, certains utilisateurs de la RSVP peuvent ressentir une fatigue oculaire ou " +
+		"du stress cognitif en raison de la rapidité et de l'intensité de la présentation. Cependant, des travaux " +
+		"récents ont cherché à atténuer ces problèmes, par exemple en introduisant des pauses stratégiques ou en " +
+		"ajustant la vitesse de présentation en fonction de la complexité du contenu.",
+
+		"+ La RSVP est une parmi de nombreuses techniques de présentation d'informations et de lecture rapide. Elle " +
+		"se distingue par sa capacité à présenter rapidement des informations dans un espace limité, mais elle a " +
+		"ses propres avantages et inconvénients en comparaison avec d'autres méthodes. Par exemple, par rapport à la " +
+		"lecture traditionnelle, la RSVP peut augmenter la vitesse de lecture, mais peut diminuer la compréhension " +
+		"et la rétention de l'information. Par rapport à d'autres méthodes de lecture rapide, la RSVP est unique en " +
+		"ce qu'elle limite les mouvements oculaires, ce qui peut réduire la fatigue, mais peut aussi limiter la " +
+		"capacité du lecteur à revenir en arrière ou à réviser l'information présentée. En fin de compte, le choix " +
+		"de la méthode dépend des besoins spécifiques de l'utilisateur, et il peut être utile d'explorer différentes " +
+		"techniques pour trouver celle qui convient le mieux.",
+
+		"+ Les développements récents et futurs de la RSVP sont prometteurs. Les chercheurs continuent d'explorer de " +
+		"nouvelles façons d'optimiser cette technique, en tenant compte de la vitesse de présentation, de la taille " +
+		"du texte, des couleurs et d'autres facteurs. Par ailleurs, avec l'avènement de nouvelles technologies " +
+		"telles que la réalité augmentée et virtuelle, la RSVP trouve de nouvelles applications, par exemple dans " +
+		"les systèmes d'information de tête haute pour les pilotes ou dans les applications de lecture sur les " +
+		"appareils mobiles. Les nouvelles recherches se concentrent également sur la personnalisation de la RSVP en " +
+		"fonction des capacités individuelles, ce qui permettrait une expérience plus confortable et efficace pour " +
+		"chaque utilisateur.",
+
+		"+ En conclusion, la Rapid Serial Visual Presentation est une technique puissante pour présenter des " +
+		"informations visuelles rapidement et efficacement. Malgré certaines limitations, telles que la possibilité " +
+		"de fatigue oculaire ou de diminution de la compréhension à des vitesses très élevées, la RSVP a une gamme " +
+		"d'applications impressionnante, de la lecture rapide à la recherche en psychologie cognitive en passant par " +
+		"les interfaces homme-machine. Alors que nous continuons à explorer et à affiner cette technique, il est " +
+		"certain que la RSVP restera un outil précieux dans notre arsenal pour traiter l'information visuelle. " +
+		"L'avenir de la RSVP est prometteur, avec de nouvelles recherches et technologies qui ouvrent des " +
+		"possibilités passionnantes pour son utilisation et son développement."
 	];
 	const [wordIndex, setWordIndex] = useState(0);
 	const [isPlaying, setIsPlaying] = useState(false);
-	const [wordPerMinute, setWordPerMinute] = useState(200);
+	const [wordPerMinute, setWordPerMinute] = useState(600);
 	const [fontSize, setFontSize] = useState(52);
 	const [textColor, setTextColor] = useState('#000000');
 	const [bgColor, setBgColor] = useState('#F5F5F5');
@@ -111,21 +131,33 @@ export default function Page() {
 
 	const chapters = [
 		{
-			label: 'Chapitre 1 - Introduction',
+			label: 'Chapitre 1 - Introduction à la Rapid Serial Visual Presentation',
 			order: 1,
 		},
 		{
-			label: 'Chapitre 2 - Les autres méthodes de lecture rapide',
+			label: 'Chapitre 2 - Principes de fonctionnement de la RSVP',
 			order: 1,
 		},
 		{
-			label: 'Hehe',
+			label: 'Chapitre 3 - Applications de la RSVP',
 			order: 1,
 		},
 		{
-			label: 'wtf',
+			label: 'Chapitre 4 - Recherches et études sur la RSVP',
 			order: 1,
-		}
+		},
+		{
+			label: 'Chapitre 5 - Comparaison de la RSVP à d\'autres méthodes',
+			order: 1,
+		},
+		{
+			label: 'Chapitre 6 - Perspectives et avenir de la RSVP',
+			order: 1,
+		},
+		{
+			label: 'Chapitre 7 - Conclusion',
+			order: 1,
+		},
 	];
 
 	const items = chapters.map((chapter, index) => (
